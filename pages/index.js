3ff51330b85cbcd9cf82e607ca17251a9eb3481e
Jotafacu890 <facu.e.moran@gmail.com>
2025-05-21 19:46:36 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,25 +19,51 @@ export default function Home() {
       </section>
 
       {/* Tecnologías */}
-      <section className="max-w-4xl text-center">
-        <h3 className="text-2xl font-semibold mb-6">Tecnologías</h3>
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-8">
-          {[
-            { name: 'Python', src: 'https://images.icon-icons.com/2699/PNG/512/python_logo_icon_168886.png' },
-            { name: 'Java', src: 'https://images.vexels.com/media/users/3/166401/isolated/lists/b82aa7ac3f736dd78570dd3fa3fa9e24-icono-del-lenguaje-de-programacion-java.png' },
-            { name: 'HTML', src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png' },
-            { name: 'CSS', src: 'https://www.superbefilms.com/wp-content/uploads/2016/03/css-logo.png' },
-            { name: 'SQL', src: 'https://brandlogos.net/wp-content/uploads/2025/03/microsoft_sql_server-logo_brandlogos.net_wykhq.png' },
-            { name: 'JavaScript', src: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png' },
-          ].map((tech) => (
-            <div
-              key={tech.name}
-              className="flex flex-col items-center transform transition-transform duration-300 hover:scale-110"
-            >
-              <img src={tech.src} alt={tech.name} className="w-16 h-16 mb-2" />
-              <p>{tech.name}</p>
-            </div>
-          ))}
+      <section className="max-w-5xl text-center space-y-12">
+        <div>
+          <h3 className="text-2xl font-semibold mb-6">Lenguajes y Frameworks</h3>
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8">
+            {[
+              { name: 'Python', src: 'https://images.icon-icons.com/2699/PNG/512/python_logo_icon_168886.png' },
+              { name: 'Java', src: 'https://images.vexels.com/media/users/3/166401/isolated/lists/b82aa7ac3f736dd78570dd3fa3fa9e24-icono-del-lenguaje-de-programacion-java.png' },
+              { name: 'HTML', src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png' },
+              { name: 'CSS', src: 'https://www.superbefilms.com/wp-content/uploads/2016/03/css-logo.png' },
+              { name: 'JavaScript', src: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png' },
+              { name: 'React', src: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg' },
+              { name: 'Redux', src: 'https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png' },
+              { name: 'Spring Boot', src: 'https://spring.io/images/icon-spring-b8754b8d2f26b0b63cf596c8d1ecb5eb.svg' },
+              { name: 'Hibernate', src: 'https://cdn.worldvectorlogo.com/logos/hibernate.svg' },
+            ].map((tech) => (
+              <div
+                key={tech.name}
+                className="flex flex-col items-center transform transition-transform duration-300 hover:scale-110"
+              >
+                <img src={tech.src} alt={tech.name} className="w-16 h-16 mb-2 object-contain" />
+                <p>{tech.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h3 className="text-2xl font-semibold mb-6">Bases de Datos</h3>
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8">
+            {[
+              { name: 'SQL', src: 'https://brandlogos.net/wp-content/uploads/2025/03/microsoft_sql_server-logo_brandlogos.net_wykhq.png' },
+              { name: 'MongoDB', src: 'https://www.svgrepo.com/show/331488/mongodb.svg' },
+              { name: 'Cassandra', src: 'https://cdn.worldvectorlogo.com/logos/apache-cassandra.svg' },
+              { name: 'Redis', src: 'https://upload.wikimedia.org/wikipedia/commons/6/6b/Redis_Logo.svg' },
+              { name: 'Neo4j', src: 'https://upload.wikimedia.org/wikipedia/commons/8/87/Neo4j_logo.svg' },
+            ].map((db) => (
+              <div
+                key={db.name}
+                className="flex flex-col items-center transform transition-transform duration-300 hover:scale-110"
+              >
+                <img src={db.src} alt={db.name} className="w-16 h-16 mb-2 object-contain" />
+                <p>{db.name}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </section>
 
@@ -83,3 +109,4 @@ export default function Home() {
     </div>
   );
 }
+
